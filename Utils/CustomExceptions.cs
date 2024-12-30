0c5a4c936ac05e22ef77784200b00e426e54b7af
dwiw96 <dwiwahyudi1996@gmail.com>
2024-12-30 15:06:00 +0700

    Add: Custom exception for not null, duplicate, and length

--- Utils/CustomExceptions.cs
+++ Utils/CustomExceptions.cs
@@ -2,3 +2,6 @@ namespace ran_product_management_net.Utils;
 
 public class NotFoundException(string message) : Exception(message);
 public class DatabaseCrudFailedException(string message) : Exception(message);
+public class NotNullException(string message) : Exception(message);
+public class DuplicateDataException(string message) : Exception(message);
+public class LengthException(string message) : Exception(message);
