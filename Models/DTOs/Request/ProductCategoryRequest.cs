0f79b6be2ba4c86ccd0ae2b8436fd4585634920c
dwiw96 <dwiwahyudi1996@gmail.com>
2024-12-31 16:03:36 +0700

    Add: Model validation for string min & max length to name parameter

--- Models/DTOs/Request/ProductCategoryRequest.cs
+++ Models/DTOs/Request/ProductCategoryRequest.cs
@@ -5,12 +5,14 @@ namespace ran_product_management_net.Models.DTOs.Request;
 public class CreateProductCategoryReq
 {
     [Required(ErrorMessage = "enter the name field")]
+    [StringLength(255, MinimumLength = 1, ErrorMessage = "{0} must be between {2} and {1} character")]
     public required string Name { get; set; }
     public string? Desc { get; set; }
 }
 
 public class UpdateProductCategoryReq
 {
+    [StringLength(255, MinimumLength = 1, ErrorMessage = "{0} must be between {2} and {1} character")]
     public string? Name { get; set; }
     public string? Desc { get; set; }
 }
