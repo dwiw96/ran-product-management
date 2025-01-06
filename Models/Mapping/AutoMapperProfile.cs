cb3142b25ebc9f8b30ac47d33a68e99db1932a2e
dwiw96 <dwiwahyudi1996@gmail.com>
2025-01-06 18:09:09 +0700

    Delete: Automapper profile for ListProductDto and GetProductDto

--- Models/Mapping/AutoMapperProfile.cs
+++ Models/Mapping/AutoMapperProfile.cs
@@ -16,8 +16,6 @@ public class AutoMapperProfile : Profile
             // .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt.ToString("yyyy-MM-dd")));
         CreateMap<UpdateProductCategoryReq, UpdateProductCategoryReq>()
             .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name));
-        CreateMap<ListProductDto, Product>();
-        CreateMap<GetProductDto, Product>();
         CreateMap<CreateProductReq, ProductInventory>();
         CreateMap<CreateProductReq, ProductDetail>();
         CreateMap<ProductInventory, ProductInventoryResp>();
