9362d8d83dbde58e69002097b0af05fde9924d60
dwiw96 <dwiwahyudi1996@gmail.com>
2025-01-06 18:18:30 +0700

    Fix & Modify: Change to repository pattern and added exception catch

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,84 +1,88 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using MongoDB.Driver;
 using ran_product_management_net.Database.Mongodb;
 using ran_product_management_net.Database.Mongodb.Models;
 using ran_product_management_net.Database.Postgresql;
 using ran_product_management_net.Database.Postgresql.Models;
 using ran_product_management_net.Models.DTOs.Request;
-using ran_product_management_net.Models.Integration;
-using System.Text.Json;
-using ran_product_management_net.Models.Mapping;
+using ran_product_management_net.Models.DTOs.Response;
+using ran_product_management_net.Services;
 using ran_product_management_net.Utils;
+using System.ComponentModel.DataAnnotations;
+using ran_product_management_net.Repositories;
 
 namespace ran_product_management_net.Controllers;
 
 [Route("api/v1/product")]
 [ApiController]
-public class ProductController(ApplicationDbContext context, MongoDBService mongoDbService, IMapper mapper) : ControllerBase
+public class ProductController(ApplicationDbContext context, MongoDBService mongoDbService, IMapper mapper, IProductRepository<ProductCategory> iCategory, IProductRepository<ProductInventory> iInventory, IProductRepository<ProductDetail> iDetail) : ControllerBase
 {
-    private readonly ApplicationDbContext _context = context;
     private readonly IMongoCollection<ProductDetail>? _productDetailCollection = mongoDbService.Database?.GetCollection<ProductDetail>("product_details");
     private readonly IMapper _mapper = mapper;
+    private readonly ProductService _service = new(context, mongoDbService, mapper, iCategory, iInventory, iDetail);
 
     [HttpGet]
     public async Task<IActionResult> ListProducts()
     {
-        var productInventory = await _context.ProductInventories.ToListAsync();
-        if (productInventory.Count == 0)
-            return NotFound();
-
-        var productDetail = await _productDetailCollection.Find(FilterDefinition<ProductDetail>.Empty).ToListAsync();
-        if (productDetail == null)
-            return NotFound();
-
-        var jsonOptions = new JsonSerializerOptions
+        SuccessWithData<List<ProductResp>> response;
+        try
         {
-            Converters = { new JsonPolymorphicConverter<ProductDetailBase>() },
-            PropertyNamingPolicy = new SnakeCaseNamingPolicy(),
-            WriteIndented = true
-        };
-
-        var productResp = ManualMapping.ToProductResponse(productInventory, productDetail, _mapper);
-
-        var response = JsonSerializer.Serialize(productResp, jsonOptions);
-
+           var results = await _service.ListAllProducts();
+           response = new SuccessWithData<List<ProductResp>>(200, "List of products", results);
+        }
+        catch (NotFoundException e)
+        {
+            SuccessWithData<List<ProductResp>> emptyResp = new(200, "there are no products data in the database", []);
+            return Ok(emptyResp);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            FailedResponse errResp = new(500,  "there is an error in the server", 
+                new Dictionary<string, List<string>>()
+                {
+                    {"server", [e.Message] } 
+                });
+            return StatusCode(500, errResp);
+        }
+        
         return Ok(response);
     }
 
     [HttpGet("{id}")]
-    public  async Task<IActionResult> GetById([FromRoute] string id)
+    
+    public  async Task<IActionResult> GetById([FromRoute] [StringLength(36, MinimumLength = 36, ErrorMessage = "ID must be a valid GUID (36 characters).")] string id)
     {
-        // if (!ModelState.IsValid)
-        //     return BadRequest(ModelState);
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
         
-        var uuid = Guid.Parse(id);
-        var productInventory = await _context.ProductInventories
-            .Include(p => p.Category)
-            .FirstOrDefaultAsync(p => p.Id == uuid);
-
-        if (productInventory == null)
+        SuccessWithData<ProductResp> response;
+        try
         {
-            return NotFound();
+            var result = await _service.GetProductById(id);
+            
+            response = new SuccessWithData<ProductResp>(200, "Product found", result);
         }
-
-        var filter = Builders<ProductDetail>.Filter.Eq(f => f.Id, uuid);
-        var productDetail = await _productDetailCollection.Find(filter).FirstOrDefaultAsync();
-        if (productDetail == null)
-            return NotFound();
-
-        var jsonOptions = new JsonSerializerOptions
+        catch (NotFoundException e)
         {
-            Converters = { new JsonPolymorphicConverter<ProductDetailBase>() },
-            PropertyNamingPolicy = new SnakeCaseNamingPolicy(),
-            WriteIndented = true
-        };
-
-        var productResp = ManualMapping.ToProductResponse(productInventory, productDetail, _mapper);
-
-        var response = JsonSerializer.Serialize(productResp, jsonOptions);
-
+            FailedResponse errResp = new(404,  $"there is no product with id {id}", 
+                new Dictionary<string, List<string>>()
+                {
+                    {"category id", [e.Message] } 
+                });
+            return new NotFoundObjectResult(errResp);
+        }
+        catch (Exception e)
+        {
+            FailedResponse errResp = new(500,  "there is an error in the server", 
+                new Dictionary<string, List<string>>()
+                {
+                    {"server", [e.Message] } 
+                });
+            return StatusCode(500, errResp);
+        }
+        
         return Ok(response);
     }
 
@@ -90,49 +94,54 @@ public class ProductController(ApplicationDbContext context, MongoDBService mong
             return BadRequest(ModelState);
         }
 
-        var category = await _context.ProductCategories.FirstOrDefaultAsync(c => c.Id == productReq.CategoryId);
-        if (category == null)
+        string productId;
+        ProductResp? productCreated;
+
+        try
         {
-            return BadRequest("product category is not found");
+            productId = await _service.CreateProduct(productReq);
+            productCreated = await _service.GetProductById(productId);
         }
-
-        var uuid = Guid.NewGuid();
-
-        var productInventory = _mapper.Map<ProductInventory>(productReq);
-        productInventory.Id = uuid;
-        await _context.ProductInventories.AddAsync(productInventory);
-
-        var productDetail = _mapper.Map<ProductDetail>(productReq);
-        productDetail.Id = uuid;
-        productDetail.Category = category.Name;
-
-        ProductDetailBase specificDetail;
-
-        switch (category.Name)
+        catch (NotFoundException e)
         {
-            case "Smartphone":
-            specificDetail = ManualMapping.ToSpecificDetail<Smartphone>(productReq.Details);
-            productDetail.SpecificDetails = specificDetail;
-            break;
-            case "Fashion":
-            specificDetail = ManualMapping.ToSpecificDetail<Fashion>(productReq.Details);
-            productDetail.SpecificDetails = specificDetail;
-            break;
-            case "Electronic":
-            specificDetail = ManualMapping.ToSpecificDetail<Electronic>(productReq.Details);
-            productDetail.SpecificDetails = specificDetail;
-            break;
+            FailedResponse errResp = new(400, "product or category was not found",
+                new Dictionary<string, List<string>>()
+                {
+                    { "message", [e.Message] }
+                });
+
+            return new BadRequestObjectResult(errResp);
         }
-        if (_productDetailCollection == null)
-            return StatusCode(500);
-
-        await _productDetailCollection.InsertOneAsync(productDetail);
-
-        await _context.SaveChangesAsync();
-
-        Product product= new();
-
-        return CreatedAtAction(nameof(GetById), new { id = uuid }, product);
+        catch (DuplicateDataException e)
+        {
+            FailedResponse errResp = new(400, "product with the same name is already in use",
+                new Dictionary<string, List<string>>()
+                {
+                    { "product_name", [e.Message] }
+                });
+            return new BadRequestObjectResult(errResp);
+        }
+        catch (KeyNotFoundException e)
+        {
+            FailedResponse errResp = new(400, "unit must be either 'Metric' or 'Imperial'",
+                new Dictionary<string, List<string>>()
+                {
+                    { "unit", [e.Message] }
+                });
+            return new BadRequestObjectResult(errResp);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            FailedResponse errResp = new(500,  "there is an error in the server", 
+                new Dictionary<string, List<string>>()
+                {
+                    {"server", [e.Message] } 
+                });
+            return StatusCode(500, e.Message);
+        }
+        
+        return CreatedAtAction(nameof(GetById), new { id = productId }, productCreated);
     }
     
     [HttpPut("{id}")]
